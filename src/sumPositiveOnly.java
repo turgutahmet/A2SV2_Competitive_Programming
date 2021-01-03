6d178f3172a052053dd270689232a83a0f1d9924
Ahmet TURGUT <turgutahmt@gmail.com>
2021-01-03 23:16:04 +0300

    sumPositiveOnly <OK>

--- src/sumPositiveOnly.java
+++ src/sumPositiveOnly.java
@@ -1,10 +1,42 @@
 public class sumPositiveOnly {
-    public static void main(String [] args){
-        String numberOne = "10^1000";
-        String numberTwo = "10^1000";
+    public static void main(String[] args) {
+        String num1 = "42131233213213";
+        String num2 = "1233123123213213";
+        StringBuilder numberOne = new StringBuilder(num1);
+        StringBuilder numberTwo = new StringBuilder(num2);
 
-        String[] arrOne = numberOne.split("\\^");
-        String[] arrTwo = numberTwo.split("\\^");
+        int iter = numberOne.length();
+        int iter2 = numberTwo.length();
+        int size = Math.max(iter, iter2);
+        StringBuilder str = new StringBuilder();
+        int can = 0;
+        for (int i = size; i >= 0; i--) {
+            int k = 0, j = 0, prev = can;
+            iter = iter - 1;
+            iter2 = iter2 - 1;
+            if (iter >= 0) {
+                k = Character.getNumericValue(numberOne.charAt(iter));
+            }
+            if (iter2 >= 0) {
+                j = Character.getNumericValue(numberTwo.charAt(iter2));
+            }
+            int sum = k + j + can;
+            if (sum >= 10) {
+                sum = sum - 10;
+                can = 1;
+            } else {
+                can = 0;
+            }
+            if (str.length() >= size) {
+                if (prev == 1) {
+                    str.append(sum);
+                }
+            } else str.append(sum);
+
+        }
+        str.reverse();
+
+        System.out.println(str);
 
 
     }
