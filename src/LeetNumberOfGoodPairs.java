3c6396797348047a922509bc464540c1955ff426
Ahmet TURGUT <turgutahmt@gmail.com>
2021-01-14 17:46:45 +0300

    NumberOfGoodPairs <Link>

--- src/LeetNumberOfGoodPairs.java
+++ src/LeetNumberOfGoodPairs.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 
+//https://leetcode.com/problems/number-of-good-pairs/
 public class LeetNumberOfGoodPairs {
     public static void main(String[] args) {
         int[] nums = {1,2,3};
