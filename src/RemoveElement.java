a319e0ca518e5d21a0d73757ccc0a4c356916fb7
turgutahmet <turgutahmt@gmail.com>
2021-07-06 23:52:17 +0300

    RemoveElement <OK>

---
+++ src/RemoveElement.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,2,3,0,4,2};
+        int val = 2;
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int ii = 0 ; ii< nums.length ; ii++){
+            if(nums[ii] != val){
+                list.add(nums[ii]);
+            }
+        }
+        int jj=0;
+        for (Integer i: list) {
+            nums[jj] = list.get(jj);
+            jj++;
+        }
+
+        System.out.println(list);
+        System.out.println(list.size());
+    }
+}
