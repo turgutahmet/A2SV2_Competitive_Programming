c18da056078b4967b7ae63151ad49836e2efc0ae
turgutahmet <turgutahmt@gmail.com>
2021-07-07 21:53:56 +0300

    SearchInsertPosition <Modify>

--- src/SearchInsertPosition.java
+++ src/SearchInsertPosition.java
@@ -1,4 +1,5 @@
 public class SearchInsertPosition {
+    //https://leetcode.com/problems/search-insert-position/
     public static void main(String[] args) {
         int[] nums = {1,3,5,6};
         int target = 0,ii = 0,output = 0;
